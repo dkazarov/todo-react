c50de4a00ef5039250c1f479a899bc6836bd6120
Dmitry KAzarov~ <d.kazarov1361@gmail.com>
2022-03-18 04:04:52 +0200

    add clissnames library

--- src/App.js
+++ src/App.js
@@ -41,15 +41,24 @@ function App() {
               color: 'pink',
               name: 'Фильмы и сериалы',
             },
+          ]}
+          isRemovable
+        />
+        <List
+          items={[
             {
-              color: '#B6E6BD',
-              name: 'Книги',
-            },
-            {
-              color: '#C9D1D3',
-              name: 'Личное',
+              className: "list__add-button",
+              icon: <svg 
+              width="12" height="12" 
+              viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"
+              >
+              <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M1 8H15" stroke="grey" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>,
+              name: 'Добавить список',
             },
           ]}
+          isRemovable
         />
       </div>
       <div className='todo__tasks'></div>
